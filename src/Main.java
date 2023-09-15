e9b502c17d1aaffca797769192b7651f41d1c61f
LandonTopy <landontopy@gmail.com>
2023-09-15 09:09:02 -0400

    Yearly Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //Variables
+        double springMain;
+        double fallMain;
+        double summerMain;
+        double winterMain;
+        double totalMain;
+
+        System.out.println("How much is the maintenance for spring?");
+        springMain = scan.nextDouble();
+        System.out.println("How much is the maintenance for summer?");
+        summerMain = scan.nextDouble();
+        System.out.println("How much is the maintenance for fall?");
+        fallMain = scan.nextDouble();
+        System.out.println("How much is the maintenance for winter?");
+        winterMain = scan.nextDouble();
+        totalMain = springMain + summerMain + fallMain + winterMain;
+        System.out.println("The total maintenance cost for the year is $" + totalMain);
     }
 }
